2da6e8fe2ed2e8abe2076740208a3362500e1a0c
Matilde <60695248+matildefaralli@users.noreply.github.com>
2024-09-18 23:55:57 +0100

    t

--- particles.js-master/particles.js
+++ particles.js-master/particles.js
@@ -49,7 +49,7 @@ var pJS = function(tag_id, params){
         random: false,
         anim: {
           enable: false,
-          speed: 2,
+          speed: 0.5,
           opacity_min: 0,
           sync: false
         }
@@ -59,7 +59,7 @@ var pJS = function(tag_id, params){
         random: false,
         anim: {
           enable: false,
-          speed: 20,
+          speed: 2,
           size_min: 0,
           sync: false
         }
